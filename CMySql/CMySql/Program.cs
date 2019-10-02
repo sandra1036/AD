ee4f08775c029fde7ced2487772e3f7d43459bff
sandra1036 <sandraromero262@gmail.com>
2019-10-02 12:16:24 +0200

    Actualizado dbprueba y CMySql

--- CMySql/CMySql/Program.cs
+++ CMySql/CMySql/Program.cs
@@ -6,26 +6,62 @@ namespace CMySql
 {
     class MainClass
     {
+      private static IDbConnection dbConnection = new MySqlConnection(
+               "server=localhost;database= dbprueba; User=root; Password=sistemas;ssl-mode=none"
+           );
+
         public static void Main(string[] args)
         {
+
             Console.WriteLine("Acceso a dbprueba");
-            IDbConnection dbConnection = new MySqlConnection(
-                "server=localhost;database= dbprueba; User=root; Password=sistemas;ssl-mode=none"
-            );
+           
 
             dbConnection.Open();
-
-
-            IDbCommand dbCommand = dbConnection.CreateCommand();
-
-            dbCommand.CommandText = "select * from categoria";
-            IDataReader dataReader= dbCommand.ExecuteReader(); 
-
+            //Insert();
+            //showAll();
+            ShowMetaInfo ();
 
             dbConnection.Close();
 
 
+        }
+        public static void showAll(){
+
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+
+            dbCommand.CommandText = "select * from categoria";
+            IDataReader dataReader = dbCommand.ExecuteReader();
+
+            while (dataReader.Read()){
+                Console.WriteLine("id={0} nombre={1}", dataReader["id"], dataReader["nombre"]);
+
+            }
+            dataReader.Close();
 
         }
+       public static void Insert(){
+
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            string nombre = "nuevo" + DateTime.Now;
+            dbCommand.CommandText = String.Format("insert into categoria(nombre) values ('{0}')", nombre);
+            dbCommand.ExecuteNonQuery();
+
+        }
+     public static void ShowMetaInfo() {
+            IDbCommand dbCommand = dbConnection.CreateCommand ();
+
+            dbCommand.CommandText = "select * from categoria";
+            IDataReader dataReader = dbCommand.ExecuteReader ();
+            Console.WriteLine ("FieldCount={0}",dataReader.FieldCount);
+
+            for (int i = 0; i < dataReader.FieldCount; i++) {
+
+                Console.WriteLine("Field {0,3}={1,-15} Type={2}",i,dataReader.GetName(i),dataReader.GetFieldType(i));
+
+            }
+
+            dataReader.Close();
+        }
+
     }
 }
--- dbprueba.sql
+++ dbprueba.sql
@@ -9,3 +9,19 @@ create table categoria(
 insert into categoria (nombre) values ('categoría 1');
 insert into categoria (nombre) values ('categoría 2');
 insert into categoria (nombre) values ('categoría 3');
+
+
+/*Ejercicio crear una tabla llamada articulos donde tiene el id,nombre y 2 campos más: precio si es desconocido lo pones como nulo y el segundo campo es: categoría unsigner,bigint.
+Menú: salir,nuevo,editar,consultar,eliminar,listar*/
+
+create table articulo(
+	id  serial primary key,
+	nombre varchar(50) unique not null,
+	precio numeric(10,2),
+	categoria bigint usigner
+	FOREIGN KEY(id) REFERENCES categoria(id)
+
+);
+
+insert into articulo (nombre,precio,categoria) values ('articulo1',10,1);
+insert into articulo (nombre,precio) values ('articulo2',);
