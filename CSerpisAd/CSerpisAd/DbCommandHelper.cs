3728d8eba52284d4ee86e015039a77436727515e
sandra1036 <sandraromero262@gmail.com>
2019-10-04 09:48:52 +0200

    Añadido Libreria

---
+++ CSerpisAd/CSerpisAd/DbCommandHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+
+namespace Serpis.Ad {
+
+    public class DbCommandHelper {
+
+        public static void AddParameter(IDbCommand dbCommand, string name, object value) {
+
+            IDbDataParameter dbDataParameter = dbCommand.CreateParameter ();
+            dbDataParameter.ParameterName = name;
+            dbDataParameter.Value = value; //"cat. 33"
+            dbCommand.Parameters.Add (dbDataParameter);
+
+        }
+
+
+    }
+
+}
---
+++ CSerpisAd/CSerpisAd/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSerpisAd {
+
+    public class MyClass {
+
+        public MyClass() {
+
+        }
+
+    }
+}
