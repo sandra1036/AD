5451c7a8569499bd0795eb523316a82e9c7923d3
sandra1036 <sandraromero262@gmail.com>
2019-10-07 10:02:27 +0200

    Añadido CGTK TreeView y Toolbar

---
+++ CGtk/CGtk/CategoriaWindow.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CGtk {
+    public partial class CategoriaWindow : Gtk.Window {
+        public CategoriaWindow() :
+                base (Gtk.WindowType.Toplevel) {
+            this.Build ();
+
+        }
+    }
+}
