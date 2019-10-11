b48b2cd2317931a8e6ac77a3772c84de80a06a8a
sandra1036 <sandraromero262@gmail.com>
2019-10-11 09:44:08 +0200

    Añadido ReaderInt

--- CSerpisAd/CSerpisAd/DbCommandHelper.cs
+++ CSerpisAd/CSerpisAd/DbCommandHelper.cs
@@ -13,8 +13,30 @@ namespace Serpis.Ad {
             dbCommand.Parameters.Add (dbDataParameter);
 
         }
+        public static int ReadInteger(string label) {
 
+            while (true) {
+                Console.Write (label);
+                string linea = Console.ReadLine ();
+                Console.WriteLine ();
+
+                try {
+                    return Int32.Parse (linea);
+
+                }
+                catch {
+                    Console.WriteLine ("Formato invalido");
+                }
+            };
+        }
+        public static int readOption(String label, string op) {
+            while (true) {
+                Console.WriteLine (label);
+                string opt = Console.ReadLine ();
+            }
+
+
+        }
 
     }
-
 }
