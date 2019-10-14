5d0e6d9a88e7e2e4228db498a3d3c1eb7186d3e4
sandra1036 <sandraromero262@gmail.com>
2019-10-14 09:05:03 +0200

    Añadido CReflection

---
+++ CReflection/CReflection/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CReflection {
+    public class Categoria {
+
+        public Categoria(ulong id,string nombre) {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        //public ulong Id { get; set; }//hacer una propìerty la manera más corta
+        //public string Nombre { get; set; }
+
+        private ulong id;//se puede encapsular con alt + enter (lo hace solo)
+        private string nombre;
+
+        public ulong Id {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nombre {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+    }
+}
---
+++ CReflection/CReflection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace CReflection {
+    class MainClass {
+        public static void Main(string[] args) {
+            Categoria categoria = new Categoria (5,"Cat 5");
+            //Console.WriteLine ("c.Id={0},c.Nombre={1}",categoria.Id,categoria.Nombre);
+            ShowPropierty  (categoria,"Id","Nombre");
+        }
+        public static void ShowPropierty(object obj) {
+            Type type= obj.GetType ();
+            Console.WriteLine ("obj.GetType()={0}",type);
+            foreach(PropertyInfo propertyInfo in type.GetProperties ()) {
+                Console.WriteLine ("propertyInfo.Name={0} propertyInfo.GetValue={1}",
+                propertyInfo.Name,propertyInfo.GetValue(obj));
+            }
+        }
+        public static void ShowPropierty(object obj,params string[] propiertyNames) {
+            Type type = obj.GetType ();
+            Console.WriteLine ("obj.GetType()={0}", type);
+            foreach (string propertyName in propiertyNames) {
+                Console.WriteLine ("propiertyName={0} Value={1}",
+                propertyName, obj.GetType().GetProperty(propertyName).GetValue(obj));
+            }
+        }
+    }
+}
