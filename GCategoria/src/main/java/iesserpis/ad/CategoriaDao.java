201d206b4f9158eaa2c922f4da13e992ee535f88
sandra1036 <sandraromero262@gmail.com>
2019-10-28 10:43:52 +0100

    Añadido GMySql como .NET

---
+++ GCategoria/src/main/java/iesserpis/ad/CategoriaDao.java
@@ -0,0 +1,18 @@
+package iesserpis.ad;
+
+public class CategoriaDao {
+	
+	
+	
+	/**
+	 * Carga la categoria con el id indicado de la base de datos o null si no existe.
+	 * @param id
+	 * @return categoria con ese id o null si no existe
+	 */
+	
+	public static Categoria load(Object id) {
+		return null;
+	}
+
+
+}
---
+++ GCategoria/src/main/java/iesserpis/ad/CategoriaMain.java
@@ -0,0 +1,11 @@
+package iesserpis.ad;
+
+public class CategoriaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+		
+
+}
